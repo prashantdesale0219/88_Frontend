09e8b671982cadca095283f08366a3ba0a9b4a1c
prashantdesale259 <prashantdesale259@gmail.com>
2025-05-29 14:35:42 +0530

    complate

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // 👈 This is critical for correct asset paths!
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
+  server: { port: 3000 },
+  build: {
+    outDir: 'dist',
+  }
 });
